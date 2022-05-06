384d04b793bcefd6a40ffe868f395a732f840202
wadebooth <wademgmt@gmail.com>
2022-05-06 11:45:52 -0400

    built an API with functions

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require("firebase-functions");
+const express = require("express");
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send("This is actually working!");
+});
+
+exports.app = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
